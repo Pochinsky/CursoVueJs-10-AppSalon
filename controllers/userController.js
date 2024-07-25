31f04ca5d9fc85662786f5e57035c8e1ee9d6520
Pochinsky <tomas.guttman@sansano.usm.cl>
2024-07-25 11:07:36 -0400

    ✨ read appointments by user added

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+import Appointment from "../models/Appointment.js";
+
+const getUserAppointments = async (req, res) => {
+  const { user } = req.params;
+  const role = "user";
+  if (user !== req.user._id.toString() && role !== "admin") {
+    const error = new Error("Acceso denegado");
+    return res.status(400).json({ message: error.message });
+  }
+  try {
+    const appointments = await Appointment.find({
+      user,
+      date: { $gte: new Date() },
+    })
+      .populate("services")
+      .sort({ date: "asc" });
+    return res.json(appointments);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { getUserAppointments };
